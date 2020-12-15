36be40455eaa5fa45069d2a912d2ffb7dc8c5f50
Alex Calixte <alexcalixte1994@gmail.com>
2020-12-15 13:44:45 -0500

    Initial Commit

---
+++ Nota/Assets/AuthManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuthManager : MonoBehaviour
+{
+    Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Nota/Assets/Telegram 3D/Telegram/Scripts/UI/Panels/Chat/ChatPanel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Telegram.Chat
+{
+    public class ChatPanel : MonoBehaviour
+    {
+        [Header("Components")] [SerializeField]
+        private Image _navBackground;
+
+        [SerializeField] private Text _textNavHeader;
+        [SerializeField] private Button _btnChat;
+
+        public event Action<string> OnClickChat;
+
+        private void Start()
+        {
+            _btnChat.onClick.AddListener(() =>
+            {
+                if (OnClickChat == null) return;
+                var nameChat = _btnChat.GetComponentInChildren<Text>().text;
+                OnClickChat(nameChat);
+            });
+        }
+
+      
+    }
+}
